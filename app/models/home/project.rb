2e6d4a15b76865e327614702285f5d7b35bd7d58
Joe Noonan <joenoonan27@gmail.com>
2014-10-24 19:00:50 -0500

    checkpoint

--- app/models/home/project.rb
+++ app/models/home/project.rb
@@ -5,4 +5,19 @@ class Home::Project < ActiveRecord::Base
   validates :description, presence: true
 
   belongs_to :coding_language
+
+  def self.get_languages
+    Home::Project.group(:language).pluck(:language).sort
+  end
+
+  def self.get_projects_with_coding_languages_hash
+    hash = {}
+
+    get_languages.each do |l|
+      hash[l] = self.where(language: l)
+    end
+
+    hash
+  end
+
 end
--- spec/models/home/project_spec.rb
+++ spec/models/home/project_spec.rb
@@ -4,4 +4,18 @@ RSpec.describe Home::Project, :type => :model do
   it { is_expected.to validate_presence_of :name}
   it { is_expected.to validate_presence_of :language}
   it { is_expected.to validate_presence_of :description}
+
+  context "#get_languages" do
+    before(:all) do
+      2.times{ |num| FactoryGirl.create(:project, language: "lang_#{num}")}
+    end
+
+    it "returns a sorted array of the languages in the DB" do
+      projects = Home::Project.get_languages
+
+      expect(projects).to match_array ["lang_1","lang_2"]
+    end
+
+
+  end
 end
