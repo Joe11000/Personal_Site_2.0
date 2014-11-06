364f1c2a8baa2b5ed33cc92904745fc5efbb171a
Joe Noonan <joenoonan27@gmail.com>
2014-11-06 08:48:58 -0600

    I dont know if this changes anything

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,6 +5,7 @@ Rails.application.config.assets.version = '1.1'
 
 Rails.application.config.assets.precompile += %w( blog/blog_stylesheet.css blog/comments.js blog/entries.js)
 Rails.application.config.assets.precompile += %w( home/homes.css home/homes.js )
+Rails.application.config.assets.precompile += %w( home/projects.css )
 Rails.application.config.assets.precompile += %w( resume/resume.css resume/resume.js )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
