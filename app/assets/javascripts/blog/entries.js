0c8dd2825da82026ee3be73f5c0776fd5f1271e9
Joe Noonan <joenoonan27@gmail.com>
2017-05-30 03:18:55 -0500

    adding some spacing to the add new comment form and added a background image

--- app/assets/javascripts/blog/entries.js
+++ app/assets/javascripts/blog/entries.js
@@ -5,3 +5,27 @@
 // $(function(){
 //   alert('blog/entries loaded')
 // });
+
+$(function(){
+
+  $("[data-id='new_comment_body']").on('input', function(e){
+    enableDisableCommentButton();
+  });
+
+  $("[data-id='new_comment_author_name']").on('input', function(e){
+    enableDisableCommentButton();
+  });
+
+  function enableDisableCommentButton() {
+    if( $("[data-id='new_comment_body']").val().length > 0 &&
+        $("[data-id='new_comment_author_name']").val().length > 0
+      )
+    {
+      $("[data-id='new_comment_submit']").removeClass('disabled')
+    }
+    else
+    {
+      $("[data-id='new_comment_submit']").addClass('disabled')
+    }
+  }
+});
