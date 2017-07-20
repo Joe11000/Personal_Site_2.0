3d506a65c36b94e5e389eb8ee2788430395939ce
Joe Noonan <joenoonan27@gmail.com>
2017-07-20 16:03:59 -0500

    recompile assets and add blog_entries folder specifically to the asset pipeline

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,6 +9,7 @@ Rails.application.config.assets.precompile += %w( home/projects.css )
 Rails.application.config.assets.precompile += %w( resume/resume.css resume/resume.js )
 
 Rails.application.config.assets.precompile += Dir.glob("#{Rails.root}/app/assets/**/")
+Rails.application.config.assets.precompile += Dir.glob("#{Rails.root}/app/assets/files/blog/entries/*")
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
