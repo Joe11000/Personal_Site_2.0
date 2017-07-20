9faf3af07449c80af37026cd78ebc85af8382ef8
Joe Noonan <joenoonan27@gmail.com>
2017-07-20 15:17:57 -0500

    try again

--- db/schema.rb
+++ db/schema.rb
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 20170530213555) do
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.datetime "created_on", default: '2017-07-20 20:07:36'
+    t.datetime "created_on", default: '2017-07-20 20:13:31'
   end
 
   create_table "home_projects", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -23,8 +23,8 @@ blog_entries_folder.each do |file|
     end
   end
   created_on = file.to_s.match(/Blog_Post_(.*)$/)[1]
-  puts created_on
-  Blog::Entry.create( title: title, body: body, created_on: DateTime.strptime(created_on, '%m.%d.%y') )
+
+  Blog::Entry.create( title: title, body: body, created_on: Date.strptime(created_on, '%m.%d.%y') )
 end
 
 
